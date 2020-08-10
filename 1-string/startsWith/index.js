2a6f051857843080b6d5b74d0d32e8b842b2b3e4
emmeem <1124782639@qq.com>
2020-08-10 19:59:01 +0800

    liao collectCarNumberCount

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let num = 0;
+  for (let i = 0; i < collection.length; i += 1) {
+    if (collection[i].indexOf('粤A') !== -1) {
+      num += 1;
+    }
+  }
+  return num;
 }
