15a40cf0fe35a50e4f5bc9b2e7d2ed31aba222e3
emmeem <1124782639@qq.com>
2020-08-10 21:56:13 +0800

    class

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -1,1 +1,14 @@
 // TODO 16: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const result = `${super.introduce()} I am a Student. I am at Class ${this.klass}.`;
+    return result;
+  }
+}
