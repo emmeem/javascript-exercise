b06cc10163aca5ba819085f0948e1af6ea54e73f
emmeem <1124782639@qq.com>
2020-08-10 20:19:03 +0800

    array filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(i => i % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((element, index, self) => self.indexOf(element) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
