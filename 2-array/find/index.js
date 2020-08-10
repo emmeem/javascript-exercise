17412d954a34b761bccd5202f074dbb813edf19f
emmeem <1124782639@qq.com>
2020-08-10 20:26:52 +0800

    array find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,5 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const tperson = collection.find(person => person.age > 10 && person.age < 20);
+  return tperson.name;
 }
