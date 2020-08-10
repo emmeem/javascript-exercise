519a05536fb94d27117253b1b9078931b11097d4
emmeem <1124782639@qq.com>
2020-08-10 21:30:55 +0800

    class 1.2.3

--- 4-class/practice_2/person.js
+++ 4-class/practice_2/person.js
@@ -1,1 +1,22 @@
 // TODO 10: 在这里写实现代码
+export default class person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+}
+
+export class student extends person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `I am a student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,13 @@
 // TODO 11: 在这里写实现代码
+import person from './person';
+
+export default class student extends person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
